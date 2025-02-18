9510a9efd33676db629e1f0171e7fe0414755d40
rayan2125 <dhananjay.s.dubey20125@gmail.com>
2025-02-18 16:40:41 +0530

    changing in the router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors"
+import router from "./router";
 dotenv.config();
 
 const app = express();
