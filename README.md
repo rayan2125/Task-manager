f3f8834f315ac10330983e2944c04e92ecf6c23b
rayan2125 <dhananjay.s.dubey20125@gmail.com>
2025-02-18 15:45:02 +0530

    changing in the given code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Task-manager
 testing
+<!-- mongodb+srv://dhananjaysdubey20125:<db_password>@task-manager.x6okf.mongodb.net/?retryWrites=true&w=majority&appName=task-manager -->
--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const App = express();
+dotenv.config();
 
-App.get("/", (req, res) => {
-    res.send("Hello, Render!");
+const app = express();
+
+// MongoDB connection
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello, MongoDB on Render!");
 });
 
 const PORT = process.env.PORT || 3000;
-
-App.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-export default App;
+export default app;
